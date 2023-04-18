1e4fdd6c9e3a99657c6b7981e665866f5d29aff0
ArnavPrakash <arnavprakash@iitbhilai.ac.in>
2023-04-18 14:02:37 +0530

    Remove inline css

--- src/components/main-section.jsx
+++ src/components/main-section.jsx
@@ -1,38 +1,15 @@
 import React from "react";
-import mainImage from "../assets/main-img.png";
-import './Why.css'
+// import mainImage from "../assets/main-img.png";
+import './main-section.css'
 
 function MainSection() {
-  const sectionStyle = {
-    display: "flex",
-    alignItems: "flex-start",
-    height: "90vh",
-    paddingTop: '5%',
 
-  };
-
-  const imageStyle = {
-    maxHeight: "80%",
-    marginLeft: "2rem",
-  };
-
-  const textheadStyle = {
-    fontSize: "4rem",
-    fontWeight: "bold",
-    marginLeft: "2rem",
-  };
-
-  const textStyle = {
-    fontSize: "2rem",
-    fontWeight: "regular",
-    marginLeft: "2rem",
-  };
   return (
     <div className="color">
-      <div className="main-section" style={sectionStyle}>
+      <div className="main-section">
         <div className="Content">
-          <p style={textheadStyle}>Innovating for your success.</p>
-          <p style={textStyle}>Your success is our success. We are committed to delivering innovative solutions and exceptional service to help you achieve your goals. Let us help you turn your vision into a reality. </p>
+          <p className="textheadStyle">Innovating for your success.</p>
+          <p className="textStyle">Your success is our success. We are committed to delivering innovative solutions and exceptional service to help you achieve your goals. Let us help you turn your vision into a reality. </p>
         </div>
         {/* <img src={mainImage} alt="Main" style={imageStyle} /> */}
       </div>
