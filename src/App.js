90c0d63755546c807807b7494135dd338b204afa
ArnavPrakash <arnavprakash@iitbhilai.ac.in>
2023-07-12 19:13:48 +0530

    changed to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import * as Components from './components';
 import './App.css';
 import Home from './Home'
@@ -9,7 +9,7 @@ import Courses from './Courses'
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
     <Routes>
       <Route path="/" element={<Components.Navbar />}>
         <Route index element={<Home />} />
@@ -18,7 +18,7 @@ function App() {
         <Route path="/courses/gis" element={<Courses />} />
       </Route>
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
   );
 }
 
