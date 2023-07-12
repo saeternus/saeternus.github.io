2278ee82ef2fb43ab30c05d5dfb9844e77a9aaaf
ArnavPrakash <arnavprakash@iitbhilai.ac.in>
2023-07-12 13:57:48 +0530

    added price

--- src/components/Brightminds.jsx
+++ src/components/Brightminds.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
-import brain from '../assets/brain.png'
-import './brightminds.css'
+import React from "react";
+import brain from "../assets/brain.png";
+import "./brightminds.css";
 
 function Brightminds() {
   return (
-    <div className='brightbox'>
-        <div className='brain'>
-            <img src={brain} alt="Bright Minds" />
-        </div>
-        <div className='brighttext'>
-            <p className='brighthead'>Bright Minds  </p>
-            <p className='brightcontent'>Lörem ipsum pokigisk operafiering. Diskapet sed megabägen. Ål fadina ism plalig om än on. Vivaren satressade sen. Sening sona. Anativ teradiktisk hunas. Kånt prototal: deryvis. Ogen biode dedöligt pafaligt agnostisocial. . Ål fadina ism plalig om än on. Vivaren satressade sen. Sening sona. Anativ teradiktisk hunas.</p>
-        </div>
-      
+    <div className="brightbox">
+      <div className="brain">
+        <img src={brain} alt="Bright Minds" />
+      </div>
+      <div className="brighttext">
+        <p className="brighthead">Innovation at our core </p>
+        <p className="brightcontent">
+          Our team can assist you in transforming your business with the latest
+          tech capabilities to stay ahead of the curve.
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Brightminds
+export default Brightminds;
