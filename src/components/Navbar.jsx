a4cf86c661ba85782f41b923d953f4ef4813fd96
ArnavPrakash <arnavprakash@iitbhilai.ac.in>
2023-07-21 11:26:07 +0530

    added home button to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,28 +7,33 @@ import Footer from "./Footer";
 function Navbar() {
   return (
     <>
-    <nav className="navbar">
-      <Link className="navbar-logo" to='/'>
-        <img src={logo} alt="Logo" className="logo" />
-        <h1>Saeternus</h1>
-      </Link>
-      <div className="navbar-links">
-        <ul>
-          <li>
-            <Link to='/courses/gis'>Courses</Link>
-          </li>
-          <div className="navbar-divider"></div>
-          <li>
-            <Link to='services'>Services</Link>
-          {/* <a href="#">What we do</a> */}
-          </li>
-        </ul>
+      <nav className="navbar">
+        <Link className="navbar-logo" to="/">
+          <img src={logo} alt="Logo" className="logo" />
+          <h1>Saeternus</h1>
+        </Link>
+        <div className="navbar-links">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <div className="navbar-divider"></div>
+            <li>
+              <Link to="/courses/gis">Courses</Link>
+            </li>
+            <div className="navbar-divider"></div>
+            <li>
+              <Link to="/services">Services</Link>
+            </li>
+          </ul>
 
-        <Link to='contact-us' className="navbar-button">Contact</Link>
-      </div>
-    </nav>
-    <Outlet/>
-    <Footer/>
+          <Link to="contact-us" className="navbar-button">
+            Contact
+          </Link>
+        </div>
+      </nav>
+      <Outlet />
+      <Footer />
     </>
   );
 }
