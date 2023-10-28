75c16acc10e33643cca892d63e21b0cf8df2f7fd
tanmay0502 <81376006+tanmay0502@users.noreply.github.com>
2023-10-28 20:46:53 +0530

    UI changes

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -2,19 +2,30 @@ import React from "react";
 import "./CourseCard.css";
 import { Link } from "react-router-dom";
 
-function CourseCard({ img, category, heading, link , mrp, offer}) {
+function CourseCard({ img, category, heading, link, mrp, offer }) {
+
+  const mrpValue = parseFloat(mrp.replace("₹", "").replace(/,/g, ""));
+  const offerValue = parseFloat(offer.replace("₹", "").replace(/,/g, ""));
+
+  const discountPercentage = isNaN(mrpValue) || isNaN(offerValue)
+    ? "N/A"
+    : Math.round(((mrpValue - offerValue) / mrpValue) * 100);
+
   return (
     <Link to={link} className="cardlink">
-      <div class="card">
-        <div class="card-image">
-          <img src={img} />
+      <div className="card">
+        <div className="discount-badge">
+          {discountPercentage === "N/A" ? "N/A" : `${discountPercentage}% OFF`}
         </div>
-        <div class="category"> {category} </div>
-        <div class="heading">
+        <div className="card-image">
+          <img src={img} alt="Course Image" />
+        </div>
+        <div className="category">{category}</div>
+        <div className="heading">
           {heading}
-          <div class="cardprice">
-            <span class="discounted">{offer}</span>
-            <span class="cardmrp">{mrp}</span>
+          <div className="cardprice">
+            <span className="discounted">{offer}</span>
+            <span className="cardmrp">{mrp}</span>
           </div>
         </div>
       </div>
