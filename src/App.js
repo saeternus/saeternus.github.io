750550c9b5f1b4def167c22d09b3346482bdec49
Shashwat Jaiswal <shashwatjaiswal2001@gmail.com>
2023-12-18 12:52:12 -0600

    Added rerouting

--- src/App.js
+++ src/App.js
@@ -20,8 +20,11 @@ function App() {
             <Route exact path="/services" element={<Services />} />
             <Route exact path="/contact-us" element={<ContactUs />} />
             <Route exact path="/courses" element={<CourseCatalog />} />
-            <Route exact path="/courses/:slug" element={<CourseLoader />} /> 
-            <Route path="/courses/*" element={<Navigate to="/" />} />
+            <Route exact path="/courses/:slug" element={<CourseLoader />} />
+            {/* Backward Compatibilty: Please do not remove the next two line. */}
+            <Route exact path="/courses/research-methodology" element={<Navigate to="/courses/research-methodology-research-design-and-writing" />} />
+            <Route exact path="/courses/remote-sensing-erdas" element={<Navigate to="/courses/remote-sensing" />} />
+            <Route path="/courses/*" element={<Navigate to="/courses" />} />
             <Route path="/*" element={<Navigate to="/" />} />
           </Route>
         </Routes>
