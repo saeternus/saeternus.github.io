197c95520cecf8fb08045d2b818cdc4432383875
tanmay0502 <81376006+tanmay0502@users.noreply.github.com>
2023-12-31 10:16:34 +0530

    hompage animations

--- src/components/Why.jsx
+++ src/components/Why.jsx
@@ -1,21 +1,32 @@
 import React from 'react'
 import './Why.css'
+import { motion, useAnimation } from "framer-motion";
 
 function Why() {
     return (
-        <div className='Why'>
-            <p className='WhyTopic'>Why Saeturnus?</p>
-            <div className='Cards'>
-                <div className='Card'>
-                    <h2>Solutions</h2>
-                    <p>Our solutions are designed to help businesses solve complex challenges and achieve their goals. Whether you need a new website, an e-commerce solution, a mobile application, digital marketing services, or technology consulting, we have the expertise and experience to help you succeed. </p>
+        <div>
+            {/* <hr className='TopLine'></hr> */}
+            <div className='Why'>
+            <motion.div
+                    initial={{ opacity: 0, scale: 0 }} 
+                    whileInView={{ opacity: 1, scale: [1.2, 1] }} 
+                    transition={{ duration: 1, }} 
+                    ><p className='WhyTopic'>Why Saeturnus?</p>         
+                </motion.div>
+                
+                <div className='Cards'>
+                    
+                    <div className='Card'>
+                        <h2>Solutions</h2>
+                        <p>Our solutions are designed to help businesses solve complex challenges and achieve their goals. Whether you need a new website, an e-commerce solution, a mobile application, digital marketing services, or technology consulting, we have the expertise and experience to help you succeed. </p>
+                    </div>
+                    <div className='Card'>
+                        <h2>Personalization</h2>
+                        <p>We understand that every business is unique and has specific needs and requirements. We offer personalized solutions that are tailored to our clients' individual needs. Our personalized approach ensures that our clients receive solutions that are tailored to their specific needs and requirements.</p>
+                    </div>
                 </div>
-                <div className='Card'>
-                    <h2>Personalization</h2>
-                    <p>We understand that every business is unique and has specific needs and requirements. We offer personalized solutions that are tailored to our clients' individual needs. Our personalized approach ensures that our clients receive solutions that are tailored to their specific needs and requirements.</p>
-                </div>
-            </div>
 
+            </div>
         </div>
     )
 }
