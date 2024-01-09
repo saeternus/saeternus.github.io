aaa4b1fdb1c51830248b9b200c381ef5d318ea57
Shashwat Jaiswal <shashwatjaiswal2001@gmail.com>
2024-01-09 14:21:01 +0530

    updated gis date

--- src/AllCourses/gis.js
+++ src/AllCourses/gis.js
@@ -2,7 +2,7 @@ const gis = {
     title: 'GIS Certification',
     pageTitle: 'GIS Certificate Program',
     slug: 'gis',
-    startdate: 'January 7, 2024',
+    startdate: 'February 2, 2024',
     header: 'Certificate Course in Geographical Information System (GIS)',
     description: 'Emerging technologies such as GIS (Geographic Information System) and RS (Remote Sensing) are instrumental in addressing intricate global challenges and impacting various aspects of life. In this context, the role of a Geospatial expert is gaining significance in the decision-making process. Acquiring practical knowledge is essential to comprehensively grasp the underlying concepts and unlock the potential for innovative applications of these technologies across diverse domains.',
     whatsapp: 'https://chat.whatsapp.com/FRmZTHwsDLACdxUAQOg3GE',
